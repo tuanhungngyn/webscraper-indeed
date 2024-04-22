e696ce30d71f6a69f62c36c3b480d3dafc8deef9
Hung <nguyen.tuan.hung@icloud.com>
2024-04-22 19:39:35 +0200

    working script

--- webscraper_indeed.py
+++ webscraper_indeed.py
@@ -11,43 +11,23 @@ html_source_code = browser.execute_script("return document.body.innerHTML;")
 html_soup = BeautifulSoup(html_source_code, "html.parser")
 
 jobs = html_soup.find_all(class_= "css-5lfssm")
-# %%
-jobs[0].text
-# %%
-jobs[1].text
-# %%
+
 for job in jobs:
-    title = job.text
-    # location = job.find("div", class_="css-92r8pb")
-    # company = job.find("span", class_="css-1p0sjhy")
-    print(title.strip(), "\n")
-    # print(location)
-    # print(company)
-    # print("\n")
-    # company = job.find("span", class_="css-1p0sjhy")
-
-
-# %%
-
-
-#titles = browser.find_elements(By.CLASS_NAME, "title")
-#company_element = browser.find_element(By.XPATH, "")
-#location_element = browser.find_element(By.XPATH, "")
-#tag_element = browser.find_element(By.XPATH, "")
-titles
-# %%
-
-
-
-
-
-# %%
-jobs[0].text
-# %%
-job_title = job.find('span', id='jobTitle-e8f1e8f5742797f5')
-if job_title:
-    print(job_title.text)
-else:
-    print("Erorr text not found")
-
+    
+    title_tag = job.find(lambda tag: tag.has_attr("title"))
+    if title_tag:
+        print(title_tag.text)
+    else:
+        print("Title not found")
+    company = job.find("span", class_="css-92r8pb")
+    if company:
+        print(company.text)
+    else:
+        print("Company not found")
+    location = job.find("div", "css-1p0sjhy")
+    if location:
+        print(location.text)
+    else:
+        print("Location not found")
+    print("\n")
 # %%
