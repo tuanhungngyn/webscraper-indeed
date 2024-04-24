9a532f311097d96b4086df324eea9bfab8438845
Hung <hun.nguyen@tuhh.de>
2024-04-24 03:30:03 +0200

    implementing turning pages

--- webscraper_indeed.py
+++ webscraper_indeed.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import csv
+import time
+
 
 browser = webdriver.Chrome()
 browser.get("https://de.indeed.com/jobs?q=werkstudent+Informatik&l=Hamburg&from=searchOnHP&vjk=74a581fd64741df8")
@@ -17,28 +19,37 @@ with open("job_listing.csv", "w", newline="") as file:
     header = ["Title", "Company", "Location"]
     writer.writerow(header)
 
+    while True:
+        time.sleep(5)
+        for job in jobs:
+            title_tag = job.find(lambda tag: tag.has_attr("title"))
+            if title_tag:
+                print(title_tag.text)
+            else:
+                continue
+            company = job.find("span", class_="css-92r8pb")
+            if company:
+                print(company.text)
+            else:
+                continue
+            location = job.find("div", "css-1p0sjhy")
+            if location:
+                print(location.text)
+            else:
+                continue
+            print("\n")
+            data = [title_tag.text, company.text, location.text]
+            writer.writerow(data)
+        try:
+            next_button = browser.find_element(By.XPATH, "/html/body/main/div/div[2]/div/div[5]/div/div[1]/nav/ul/li[5]/a")
+            if next_button:
+                next_button.click()
+        except Exception as e:
+            print("Failed to click on the next button or end of pages", str(e))
+            break
 
 
-    for job in jobs:
-        
-        title_tag = job.find(lambda tag: tag.has_attr("title"))
-        if title_tag:
-            print(title_tag.text)
-        else:
-            continue
-        company = job.find("span", class_="css-92r8pb")
-        if company:
-            print(company.text)
-        else:
-            continue
-        location = job.find("div", "css-1p0sjhy")
-        if location:
-            print(location.text)
-        else:
-            continue
-        print("\n")
 
-        data = [title_tag.text, company.text, location.text]
-        writer.writerow(data)
+browser.quit()
 
 # %%
