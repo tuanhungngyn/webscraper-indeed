65745168205b96e1bd40f536cbe5ab32b2fb2a12
Hung <nguyen.tuan.hung@icloud.com>
2024-06-04 21:07:41 +0200

    divided code

---
+++ scraping.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+
+
+def parse_html_joblist(browser):
+    html_source_code = browser.execute_script("return document.body.innerHTML;")
+    html_soup = BeautifulSoup(html_source_code, "html.parser")
+    jobs = html_soup.find_all(class_="css-5lfssm")
+     
+    new_data = []
+
+    #look if jobs are properly found
+    print(f"Found jobs: {len(jobs)} on current page")
+     
+    
+    for job in jobs:
+
+        #debugging cases
+        #print("HTML: ", job.prettify())
+
+        title_tag = job.find(lambda tag: tag.has_attr("title"))
+        company = job.find("span", class_="css-92r8pb")
+        location = job.find("div", class_="css-1p0sjhy")
+        job_link_tag = job.find("a", class_="jcs-JobTitle css-jspxzf eu4oa1w0")
+
+        #for debugging cases
+        # print("Title: ", title_tag)
+        # print("Company: ", company)
+        # print("Location: ", location)
+        # print("Job Information: ", job_link_tag)
+        
+        if title_tag and company and location and job_link_tag:
+            relative_job_url = job_link_tag["href"]
+            full_job_url = "de.indeed.com" + relative_job_url
+            id_number += 1
+            data = {
+                "ID": id_number,
+                "Title": title_tag.text.strip(),
+                "Location": location.text.strip(),
+                "Job Information": full_job_url
+            }
+            new_data.append(data)
+            print("New jobs: ", data)
+    
+    return new_data, id_number
--- webscraper_indeed.py
+++ webscraper_indeed.py
@@ -1,116 +1,43 @@
 #%%
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
-import os
 import time
-import pandas as pd
-
-job_title = input("What Job are you looking for? :")
-location = input("Where do you want to work? :")
-# Replace the the input with + so URL is properly working
-job_title = job_title.replace(" ", "+")
-location = location.replace(" ", "+")
-url = f"https://de.indeed.com/jobs?q={job_title}&l={location}"
-
-browser = webdriver.Chrome()
-browser.get(url)
-input_file = f"{job_title}_{location}.csv"
-
-file_exists = os.path.exists(input_file)
-if file_exists:
-    df = pd.read_csv(input_file)
-else:
-    df = pd.DataFrame(columns=["ID", "Title", "Company", "Location", "Job Information"])
-
-id_number = len(df) #if there is already an existing file to append
-page_number = 0
+from init_browser import init_browser, next_page
+from browser_using import decline_cookies, save_to_csv, id_number_declaration
+from scraping import parse_html_joblist
 
 
-while True:
-    time.sleep(5)  #wait for the page to load every element
+def main():
+    job_title = input("What Job are you looking for? :")
+    location = input("Where do you want to work? :")
+    job_title = job_title.replace(" ", "+")
+    location = location.replace(" ", "+")
+    url = f"https://de.indeed.com/jobs?q={job_title}&l={location}"
 
-    if page_number == 0:
-        try:
-            cookies_button = WebDriverWait(browser, 10).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, "#onetrust-reject-all-handler")) 
-            )
-            cookies_button.click()
-            print("Cookies declined on the first page.")
-        except Exception as e:  
-            print("Cookies button not found on the first page:", str(e))
-    else:
-        try:
-            
-            overlay_close_button = WebDriverWait(browser, 10).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, "#mosaic-desktopserpjapopup > div.css-g6agtu.eu4oa1w0 > button > svg"))
-            )
-            overlay_close_button.click()
-            print(f"Overlay closed on page {page_number + 1}.")
-        except Exception as e:
-            print(f"Overlay button not found on page {page_number + 1}:", str(e))
 
-    html_source_code = browser.execute_script("return document.body.innerHTML;")
-    html_soup = BeautifulSoup(html_source_code, "html.parser")
-    jobs = html_soup.find_all(class_="css-5lfssm")
-    
-    #look if jobs are properly found
-    print(f"Found jobs: {len(jobs)} on page {page_number + 1}")
+    input_file = f"{job_title}_{location}.csv"
+    id_number = id_number_declaration(input_file)
+    page_number = 0
+    browser = init_browser(url)
 
-    new_data = []
-
-    #scrape out elements and go through every element      
-    for job in jobs:
-
-        #debugging cases
-        #print("HTML: ", job.prettify())
-
-        title_tag = job.find(lambda tag: tag.has_attr("title"))
-        company = job.find("span", class_="css-92r8pb")
-        location = job.find("div", class_="css-1p0sjhy")
-        job_link_tag = job.find("a", class_="jcs-JobTitle css-jspxzf eu4oa1w0")
-
-        #for debugging cases
-        # print("Title: ", title_tag)
-        # print("Company: ", company)
-        # print("Location: ", location)
-        # print("Job Information: ", job_link_tag)
+    while True:
+        time.sleep(5)
+        decline_cookies(browser, page_number)
+        new_data, id_number = parse_html_joblist(browser, id_number)
+        if new_data:
+            save_to_csv(new_data, input_file)
         
-        if title_tag and company and location and job_link_tag:
-            relative_job_url = job_link_tag["href"]
-            full_job_url = "de.indeed.com" + relative_job_url
-            id_number += 1
-            data = {
-                "ID": id_number,
-                "Title": title_tag.text.strip(),
-                "Location": location.text.strip(),
-                "Job Information": full_job_url
-            }
-            new_data.append(data)
-            print("New jobs: ", data)
-    
-    if new_data:
-        new_df = pd.DataFrame(new_data)
-        df = pd.concat([df, new_df], ignore_index=True)
+        if not next_page(browser):
+            break
+        
+        page_number += 1
 
-
-    # Turning to next page
-    try:
-        next_button = WebDriverWait(browser, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//a[contains(@aria-label, 'Next Page')]"))
-        )
-        next_button.click()
-        page_number += 1  # Increment the page counter after successfully clicking the next button
-        print(f"Navigated to page {page_number + 1}.")
-    except Exception as e:
-        print("Failed to click on the next button or end of pages:", str(e))
-        break
+    browser.quit()
 
 
 
-df.to_csv(input_file, index=False)
-browser.quit()
+if __name__ == "__main__":
+    main()
+#%%
 
-# %%
+
+
+
